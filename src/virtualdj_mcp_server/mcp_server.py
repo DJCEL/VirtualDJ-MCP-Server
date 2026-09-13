1b25ee333e30d3bbde5b926ce9892bf4feef44e5
DJCEL <16272026+DJCEL@users.noreply.github.com>
2026-09-13 22:30:38 +0200

    update

--- src/virtualdj_mcp_server/mcp_server.py
+++ src/virtualdj_mcp_server/mcp_server.py
@@ -14,28 +14,29 @@ console = Console()
 class VirtualDJMCPServer:
     def __init__(self):
         self.vdj_client = VirtualDJClient()
-        self._create_mcp_server()
+        self.mcp = self._create_mcp_server()
     #------------------------------------------------------------------------------------
     def _create_mcp_server(self):
-        self.mcp = FastMCP("VirtualDJ-MCP-Server",
+        mcp = FastMCP("VirtualDJ-MCP-Server",
                       instructions="Provides a bridge to communicate with VirtualDJ.",
                       on_duplicate="warn")
 
-        self._register_tools()
-        self._register_routes()
+        self._register_tools(mcp)
+        self._register_routes(mcp)
+
+        return mcp
     #------------------------------------------------------------------------------------
     def get_mcp_server(self):
         return self.mcp
     #------------------------------------------------------------------------------------
     def run_mcp_server(self):
         console.print("VirtualDJ-MCP-Server starting...")
-        mcp = self.mcp
-
+ 
         try:
             if MCP_SERVER_TRANSPORT == "stdio":
-                mcp.run()
+                self.mcp.run()
             elif MCP_SERVER_TRANSPORT == "http":
-                mcp.run(transport=MCP_SERVER_TRANSPORT.lower(), host=MCP_SERVER_HOST, port=MCP_SERVER_PORT, path=MCP_SERVER_DEFAULT_PATH)
+                self.mcp.run(transport=MCP_SERVER_TRANSPORT.lower(), host=MCP_SERVER_HOST, port=MCP_SERVER_PORT, path=MCP_SERVER_DEFAULT_PATH)
             else:
                 console.print(f"MCP_SERVER_TRANSPORT error.")
         except KeyboardInterrupt:
@@ -44,10 +45,9 @@ class VirtualDJMCPServer:
             console.print(f"MCP Server error: {e}")
             raise
         finally:
-            console.print("VirtualDJ-MCP MCP Server stopped")
+            console.print("VirtualDJ-MCP MCP Server stopped") 
     #------------------------------------------------------------------------------------
-    def _register_routes(self):
-        mcp = self.mcp
+    def _register_routes(self, mcp: FastMCP):
         #--------------------------------------------------------------------------------
         @mcp.custom_route("/", methods=["GET"])
         async def api_root(request: Request) -> PlainTextResponse:
@@ -61,14 +61,14 @@ class VirtualDJMCPServer:
         async def api_mcp(request: Request) -> PlainTextResponse:
             return PlainTextResponse("VirtualDJ-MCP-Server/mcp")       
     #------------------------------------------------------------------------------------
-    def _register_tools(self):
-       mcp = self.mcp
-       #--------------------------------------------------------------------------------
-       mcp.add_tool(self.send_VirtualDJ)
+    def _register_tools(self, mcp: FastMCP):
+       mcp.add_tool(self.send_vdjscript)
+       mcp.add_tool(self.get_vdjscript)
+       mcp.add_tool(self.play)
        mcp.add_tool(self.set_crossfader)
     #------------------------------------------------------------------------------------
     @tool
-    async def send_VirtualDJ(self, vdjscript: str) -> bool:
+    async def send_vdjscript(self, vdjscript: str) -> bool:
         """
         This tool sends a command to VirtualDJ via a vdjscript.
         It enables VirtualDJ to do an action defined by the vdjscript.
@@ -79,14 +79,41 @@ class VirtualDJMCPServer:
                 console.print(f"vdj_client.send_async({vdjscript}) => {result}")
                 return result
         except Exception as e:
-            console.print(f"Error in send_VirtualDJ: {e}")
+            console.print(f"Error in send_vdjscript: {e}")
+    #------------------------------------------------------------------------------------
+    @tool
+    async def get_vdjscript(self, vdjscript: str) -> bool:
+        """
+        This tool queries VirtualDJ via a vdjscript.
+        It enables to get and extract data from VirtualDJ.
+        """
+        try:
+            async with self.vdj_client:
+                result = await self.vdj_client.send_async(vdjscript)
+                console.print(f"vdj_client.send_async({vdjscript}) => {result}")
+                return result
+        except Exception as e:
+            console.print(f"Error in get_vdjscript: {e}")
+    #------------------------------------------------------------------------------------
+    @tool
+    async def play(self, deck_ref:str) -> bool:
+        """
+        This tool plays a song on a defined deck
+        """
+        vdjscript = f"deck {deck_ref} play"
+        try:
+            async with self.vdj_client:
+                result = await self.vdj_client.send_async(vdjscript)
+                console.print(f"vdj_client.send_async({vdjscript}) => {result}")
+                return result
+        except Exception as e:
+            console.print(f"Error in play: {e}")
     #------------------------------------------------------------------------------------
     @tool
     async def set_crossfader(self, position: float) -> bool:
         """
         This tool sets the crossfader in VirtualDJ at a certain position
         """
-        position = self.slider_clamp(position)
         vdjscript = f"crossfader {position}%"
 
         try:
@@ -96,9 +123,3 @@ class VirtualDJMCPServer:
                 return result
         except Exception as e:
             console.print(f"Error in set_crossfader: {e}")
-    #------------------------------------------------------------------------------------------------------------------------------------
-    @staticmethod
-    def _slider_clamp(x: float) -> float:
-            # Clamp x to valid range [0-100]
-            return max(0, min(100, x))
-
