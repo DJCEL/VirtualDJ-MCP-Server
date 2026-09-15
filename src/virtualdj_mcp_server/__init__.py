58bb111ebb2233f4526bcb14f0cbe38c87aefb19
DJCEL <16272026+DJCEL@users.noreply.github.com>
2026-09-15 10:13:13 +0200

    update

--- src/virtualdj_mcp_server/__init__.py
+++ src/virtualdj_mcp_server/__init__.py
@@ -1,11 +1,13 @@
 __version__ = "1.0.1"
 
 from .mcp_server import VirtualDJMCPServer
+from .mcp_server import __version__ as __virtualdjmcpserver_version__
 from .virtualdj_client import VirtualDJClient
 from .virtualdj_client import __version__ as __virtualdjclient_version__
 
 __all__ = ["__version__",
            "VirtualDJMCPServer",
            "VirtualDJClient",
+           "__virtualdjmcpserver_version__",
            "__virtualdjclient_version__"
            ]
